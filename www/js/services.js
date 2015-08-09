f5fbb9ac2d4c48bdfd1600b8620c4cf83dec9f4d
CStrix <pedronobrega@gmail.com>
2015-08-09 17:46:47 +0000

    First commit

---
+++ www/js/services.js
@@ -0,0 +1,29 @@
+angular.module('starter.services', [])
+
+.factory('Chats', function($http) {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var chats = [];
+  
+  return {
+    all:
+      function(){
+			return $http.get("http://esffranco.eu/api/users").then(function(response){
+				chats = response;
+				return chats;
+			});
+    },
+    remove: function(chat) {
+      chats.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < chats.length; i++) {
+        if (chats[i].id === parseInt(chatId)) {
+          return chats[i];
+        }
+      }
+      return null;
+    }
+  };
+});
